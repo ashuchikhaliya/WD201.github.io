5422bbb9fb484b241e645c443a7efb53414487f2
ashuchikhaliya <112147715+ashuchikhaliya@users.noreply.github.com>
2023-02-20 15:31:02 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,48 +1,55 @@
-const http = require('http');
+
 const fs = require('fs');
+const http = require('http');
 const portno = require('minimist')(process.argv.slice(2));
 
 let homePage;
-let projectPage;
 let registrationPage;
+let projectPage;
 let registrationScript;
 
+
 fs.readFile("home.html", (err, data) => {
-  if (err) throw err;
-  homePage = data.toString();
+    if (err) throw err;
+    homePage = data.toString();
 })
 
 fs.readFile("project.html", (err, data) => {
-  if (err) throw err;
-  projectPage = data.toString();
+    if (err) throw err;
+    projectPage = data.toString();
 })
 
 fs.readFile("registration.html", (err, data) => {
-  if (err) throw err;
-  registrationPage = data.toString();
+    if (err) throw err;
+    registrationPage = data.toString();
 })
 
 fs.readFile("script.js", (err, data) => {
-  if (err) throw err;
-  registrationScript = data.toString();
+    if (err) throw err;
+    registrationScript = data.toString();
 })
 
-//app.get() method to define routes for the project and registration pages. 
-//res.send() method to send the appropriate HTML content back to the client.
-const express = require('express');
-const app = express();
 
-// Define a route for the project page
-app.get('/project', (req, res) => {
-  res.send(projectPage);
-});
+http.createServer((request, response) => {
+    let url = request.url;
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    switch (url) {
 
-// Define a route for the registration page
-app.get('/registration', (req, res) => {
-  res.send(registrationPage);
-});
-
-// Start the server
-app.listen(portno.port, () => {
-  console.log(`Server listening on port ${portno.port}`);
-});
+        case "/project":
+            response.write(projectPage);
+            response.end();
+            break;
+        case "/registration":
+            response.write(registrationPage);
+            response.end();
+            break;
+        case "/script.js":
+            response.write(registrationScript);
+            response.end();
+            break;
+        default:
+            response.write(homePage);
+            response.end();
+            break;
+    }
+}).listen(portno.port);
